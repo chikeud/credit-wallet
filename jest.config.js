c7fd7c6d711c4cc2514af68107410b27b74c856f
chikeud <chikeudenze@gmail.com>
2025-05-21 11:36:11 +0100

    need more descriptive errors

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
   testMatch: ['**/tests/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
 };
